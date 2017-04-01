b526a8e4290c3a3be606c301bf402e6f6db72c9a
qlh <569374939@qq.com>
2017-04-01 09:03:26 +0800

    20170401修改

--- js/common.js
+++ js/common.js
@@ -1,7 +1,20 @@
-//user-list显示隐藏
+//下拉菜单
+$(function() {
+	$(".solution").hover(function() {
+		$(".solution-menu").toggle()
+	})
+	$(".avatar-wrap").hover(function() {
+		$(".use-list").toggle()
+	})
+})
+//header变换
 $(function(){
-	$(".nav-user-con").hover(function(){
-		$(".user-list").toggle();
+	$(window).scroll(function(){
+		if($(window).scrollTop()>=$(".header").height()){
+			$(".header").removeClass("transparent")
+		}else{
+			$(".header").addClass("transparent")
+		}
 	})
 })
 //选择服务
